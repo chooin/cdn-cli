2f77f3e725c9a99d52a0579ae8e4e7ae82f040b5
zhuning <chooin@126.com>
2017-09-24 23:09:58 +0800

    add: // 建议使用 AccessKey 子用户

--- deploy.js
+++ deploy.js
@@ -8,6 +8,7 @@ let config = {
     region: '', // OSS 所在的区域，如：oss-cn-hangzhou
     bucket: '' // 填写你在阿里云申请的 Bucket，如：movin-h5
   },
+  // 建议使用 AccessKey 子用户
   accessKey: {
     id: '', // 填写阿里云提供的 Access Key ID，如：LTAIR1m312sdawwq
     secret: '' // 填写阿里云提供的 Access Key Secret，如：v96wAI0Gkx2qVcEO2F1V31231
