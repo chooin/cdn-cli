5beb1d60c1a59a22a9a1da6c2b2bccf365a9de7b
Devin.Zhu <chooin@126.com>
2021-12-06 15:43:08 +0800

    chore: github actions

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,16 @@
 import * as path from 'path';
-import glob from 'glob'
-import dirGlob from 'dir-glob'
-import _ from 'lodash'
-import {logger} from '../utils';
-import {isFileSync} from '../utils/file';
+import glob from 'glob';
+import dirGlob from 'dir-glob';
 import minimatch from 'minimatch';
+import _ from 'lodash';
+import { logger } from '../utils';
+import { isFileSync } from '../utils/file';
+
+export enum Type {
+  Aliyun = 'aliyun',
+  Qiniu = 'qiniu',
+  Tencent = 'tencent',
+}
 
 export interface File {
   from: string;
@@ -25,30 +31,31 @@ export interface Rule {
 }
 
 export type Aliyun = {
-  type: 'aliyun';
+  type: Type.Aliyun;
   region: string;
   bucket: string;
   accessKeyId: string;
   accessKeySecret: string;
-}
+};
 
 export type Qiniu = {
-  type: 'qiniu';
+  type: Type.Qiniu;
   region: string;
   bucket: string;
   accessKey: string;
   secretKey: string;
-}
+};
 
 export type Tencent = {
-  type: 'tencent';
+  type: Type.Tencent;
   region: string;
   bucket: string;
+  appId: string;
   secretId: string;
   secretKey: string;
-}
+};
 
-export type Environment = Aliyun | Qiniu | Tencent
+export type Environment = Aliyun | Qiniu | Tencent;
 
 export interface Config {
   rules: Rule[];
@@ -60,11 +67,11 @@ export interface Config {
 
 const defaultConfig = (): Config => {
   return {
-    ...require(path.resolve(process.cwd(), './cdn.config'))
-  }
-}
+    ...require(path.resolve(process.cwd(), './cdn.config')),
+  };
+};
 
-export const config = defaultConfig()
+export const config = defaultConfig();
 
 /**
  * 遍历目录
@@ -72,37 +79,42 @@ export const config = defaultConfig()
  */
 const walk = (from): Promise<string[]> => {
   return new Promise((resolve) => {
-    glob(
-      from,
-      {},
-      (err, matches) => {
-        if (err) {
-          logger.error(err.message)
-          process.exit(1)
-        }
-        resolve(matches)
+    glob(from, {}, (err, matches) => {
+      if (err) {
+        logger.error(err.message);
+        process.exit(1);
       }
-    )
-  })
-}
+      resolve(matches);
+    });
+  });
+};
 
 // 配置 rules 的 files
 const setFiles = async () => {
   await Promise.all(config.rules.map((rule) => walk(rule.from))).then((res) => {
     res.map((files, index) => {
       config.rules[index].files = files.map((file) => {
-        const fullPath = path.resolve('.', file)
+        const fullPath = path.resolve('.', file);
         // 判断是不是文件
-        const isFile = isFileSync(fullPath)
+        const isFile = isFileSync(fullPath);
         // ignore 处理
-        const ignore = config.rules[index].ignore.some(i => minimatch(file, i))
+        const ignore = config.rules[index].ignore.some((i) =>
+          minimatch(file, i),
+        );
         // lastUpload 处理
-        const lastUpload = config.rules[index].lastUpload.some(i => minimatch(file, i))
+        const lastUpload = config.rules[index].lastUpload.some((i) =>
+          minimatch(file, i),
+        );
         // noCache 处理
-        const noCache = config.rules[index].noCache.some(i => minimatch(file, i))
-        let to = path.join(config.rules[index].to, file.replace(path.dirname(config.rules[index].from), ''))
+        const noCache = config.rules[index].noCache.some((i) =>
+          minimatch(file, i),
+        );
+        let to = path.join(
+          config.rules[index].to,
+          file.replace(path.dirname(config.rules[index].from), ''),
+        );
         if (to.indexOf('/') === 0) {
-          to.replace('/', '')
+          to.replace('/', '');
         }
         return {
           from: fullPath,
@@ -111,30 +123,76 @@ const setFiles = async () => {
           ignore,
           lastUpload,
           noCache,
-        }
-      }) as File[]
-    })
-  })
-}
+        };
+      }) as File[];
+    });
+  });
+};
+
+const getProcessEnv = (environment, name) => {
+  return process.env[`${environment}_${name}`.toUpperCase()];
+};
 
 export const setConfig = async (environment) => {
-  config.environment = config.environments[environment]
+  config.environment = config.environments[environment];
   if (config.environment) {
     delete config.environments;
+    // 支持 Github
+    if (config.environment.type === Type.Aliyun) {
+      const { region, bucket, accessKeyId, accessKeySecret } =
+        config.environment;
+      config.environment = {
+        ...config.environment,
+        region: region ? region : getProcessEnv(environment, 'region'),
+        bucket: bucket ? bucket : getProcessEnv(environment, 'bucket'),
+        accessKeyId: accessKeyId
+          ? accessKeyId
+          : getProcessEnv(environment, 'accessKeyId'),
+        accessKeySecret: accessKeySecret
+          ? accessKeySecret
+          : getProcessEnv(environment, 'accessKeySecret'),
+      };
+    }
+    if (config.environment.type === Type.Qiniu) {
+      const { region, bucket, accessKey, secretKey } = config.environment;
+      config.environment = {
+        ...config.environment,
+        region: region ? region : getProcessEnv(environment, 'region'),
+        bucket: bucket ? bucket : getProcessEnv(environment, 'bucket'),
+        accessKey: accessKey
+          ? accessKey
+          : getProcessEnv(environment, 'accessKey'),
+        secretKey: secretKey
+          ? secretKey
+          : getProcessEnv(environment, 'secretKey'),
+      };
+    }
+    if (config.environment.type === Type.Tencent) {
+      const { region, bucket, appId, secretId, secretKey } = config.environment;
+      config.environment = {
+        ...config.environment,
+        region: region ? region : getProcessEnv(environment, 'region'),
+        bucket: bucket ? bucket : getProcessEnv(environment, 'bucket'),
+        appId: appId ? appId : getProcessEnv(environment, 'appId'),
+        secretId: secretId ? secretId : getProcessEnv(environment, 'secretId'),
+        secretKey: secretKey
+          ? secretKey
+          : getProcessEnv(environment, 'secretKey'),
+      };
+    }
   } else {
-    logger.error('错误，请检查 cdn.config.js 中 environment 是否存在')
-    process.exit(1)
+    logger.error('错误，请检查 cdn.config.js 中 environment 是否存在');
+    process.exit(1);
   }
-  config.rules = config
-    .rules
-    .filter(item => {
+  config.rules = config.rules
+    .filter((item) => {
       if (item.from && item.to) {
         return true;
       }
-      logger.error('错误，必须配置 from 和 to')
-      process.exit(1)
+      logger.error('错误，必须配置 from 和 to');
+      process.exit(1);
     })
-    .map(({from, to, ignore, lastUpload, noCache, ...args}) => {
+    .map(({ from, to, ignore, lastUpload, noCache, ...args }) => {
       return {
         ...args,
         from: _.first(dirGlob.sync(from ?? [])),
@@ -142,10 +200,10 @@ export const setConfig = async (environment) => {
         ignore: dirGlob.sync(ignore ?? []),
         lastUpload: dirGlob.sync(lastUpload ?? []),
         noCache: dirGlob.sync(noCache ?? []),
-      }
-    }) as Rule[]
+      };
+    }) as Rule[];
 
   await setFiles();
 
   return config;
-}
+};
